e6ea5845094fd663272c676ff28a5c817b745114
KhadijaMomar <souarekhadija12@gmail.com>
2026-04-13 20:56:24 +0200

    responsive

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\CategoryRepository;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
+    #[Route('/', name: 'app_home')]
+    public function index(CategoryRepository $repo): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'categories' => $repo->findAll(),
+        ]);
+    }
+
+   /* #[Route('/home', name: 'home')]
     public function index(): Response
     {
         return $this->redirectToRoute('app_categories');
-    }
+    }*/
 }
